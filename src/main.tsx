038a4c962bfd48d7873cbb29232d3c3d9514168e
Rubens Pinheiro <torettodev@gmail.com>
2023-07-10 11:57:41 -0300

    aula 02 parte 01

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Login } from './views/Login.tsx'
 
+import './assets/styles/global.scss'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Login />
